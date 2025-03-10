57352868f8ec2cadcad23d3d9e1bdd760dcd2640
Vladyslav Bedryk <vladikbedrikl@gmail.com>
2025-03-10 19:23:09 +0100

    implemented interfaces to create a list of specific machines

--- src/main/java/core/mate/academy/service/MachineService.java
+++ src/main/java/core/mate/academy/service/MachineService.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * Parameterize this service and add its implementation in a separate class.
  */
-public interface MachineService <Machine>{
+public interface MachineService <T extends Machine>{
 
     /**
      * Return the list of machines.
@@ -18,7 +18,7 @@ public interface MachineService <Machine>{
      * @param type - any class of Machine sub class. For example: Truck.class or Bulldozer.class
      * @return the list of machines
      */
-    List<? extends Machine> getAll(Class <? extends Machine> type);
+    List<T> getAll(Class <? extends T> type);
 
     /**
      * Fill the machines list with passed value
@@ -28,7 +28,7 @@ public interface MachineService <Machine>{
      * @param machines - list of machines to be filled with value
      * @param value    - any object of machine sub class
      */
-    void fill(List<? super Machine> machines, Machine value);
+    void fill(List<? super T> machines, T value);
 
     /**
      * Call the method doWork() from each machine.
@@ -36,5 +36,5 @@ public interface MachineService <Machine>{
      *
      * @param machines - the list of machines
      */
-    void startWorking(List<? extends Machine> machines);
+    void startWorking(List<? extends T> machines);
 }
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -6,35 +6,36 @@ import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl implements MachineService<Machine> {
+public class MachineServiceImpl <T extends Machine> implements MachineService<T> {
 
     @Override
-    public List<Machine> getAll(Class<? extends Machine> type) {
-        List<Machine> machines = new ArrayList<>();
+    public List<T> getAll(Class<? extends T> type) {
+        List<T> machines = new ArrayList<>();
         if (type == Bulldozer.class) {
-            machines.addAll(new BulldozerProducer().get());
+            machines.addAll((Collection<? extends T>) new BulldozerProducer().get());
         } else if (type == Excavator.class) {
-            machines.addAll(new ExcavatorProducer().get());
+            machines.addAll((Collection<? extends T>) new ExcavatorProducer().get());
         } else if (type == Truck.class) {
-            machines.addAll(new TruckProducer().get());
+            machines.addAll((Collection<? extends T>) new TruckProducer().get());
         }
-        return new ArrayList<>();
+        return machines;
     }
 
     @Override
-    public void fill(List<? super Machine> machines, Machine value) {
+    public void fill(List<? super T> machines, T value) {
         for (int i = 0; i < machines.size(); i++) {
             machines.set(i, value);
         }
     }
 
     @Override
-    public void startWorking(List<? extends Machine> machines) {
+    public void startWorking(List<? extends T> machines) {
         for (Machine machine : machines) {
             machine.doWork();
         }
